08296399b53913ce0cc8f84adbcab9dffe99b31f
Raúl Villalobos <raul.villalobosvega@ucr.ac.cr>
2026-01-05 21:17:05 -0600

    Añadiendo clase con función de pérdida usando Cross Cathegorical Entrophy

--- red_neuronal.py
+++ red_neuronal.py
@@ -38,6 +38,13 @@ class Loss_CategorialCrossentropy(Loss):
         samples = len(y_pred)
         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
 
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+    
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2,3)
